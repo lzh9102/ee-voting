5dcff0f413901d309c41104e9dd366fefe61dfbc
Che-Huai Lin <lzh9102@gmail.com>
2015-03-06 23:03:18 +0800

    Rename model "VoteEvent" to "VotingEvent"

--- voting/models.py
+++ voting/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-class VoteEvent(models.Model):
+class VotingEvent(models.Model):
     title = models.CharField(max_length=256, verbose_name=_("Title"))
+    description = models.TextField(blank=True, verbose_name=_("Description"))
     expiration_date = models.DateField(verbose_name=_("Expiration Date"))
 
 class Candidate(models.Model):
-    event = models.ForeignKey(VoteEvent)
+    event = models.ForeignKey(VotingEvent)
     full_name = models.CharField(max_length=128, verbose_name=_("Full Name"))
 
 class Voter(models.Model):
@@ -15,6 +16,6 @@ class Voter(models.Model):
     passphrase = models.CharField(max_length=128, verbose_name=_("Passphrase"))
 
 class Vote(models.Model):
-    event = models.ForeignKey(VoteEvent)
+    event = models.ForeignKey(VotingEvent)
     voter = models.ForeignKey(Voter)
     choice = models.ForeignKey(Candidate, blank=True, null=True, on_delete=models.SET_NULL)
