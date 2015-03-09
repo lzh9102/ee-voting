84d388985311ed76265e233638b746bcb8fc5b80
Che-Huai Lin <lzh9102@gmail.com>
2015-03-09 23:25:53 +0800

    welcome_page: clear the form on incorrect username/passphrase

--- voting/views.py
+++ voting/views.py
@@ -156,7 +156,7 @@ class WelcomePage(FormView):
 
         # validation error, display the form again
         context = {
-            'form': self.form_class(),
+            'form': self.form_class(initial={'username': '', 'passphrase': ''}),
             'error': _("The username or passphrase you input is invalid"),
         }
         return render(self.request, self.template_name, context)
